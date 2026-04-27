6a651692314588d912c138ea47442d25a473d0cf
marco-marci <marcomarci993@gmail.com>
2026-04-27 20:51:51 +0200

    Enhance mutation handling and output for DNA sequences
    Now the program is able to operate with 2 different DNA sequences and also it compares them to understand which one is the most susceptible to radiations' damages by comparing the number of purines (that are more exposed to damages)

--- DNA-radiation-simulator.c
+++ DNA-radiation-simulator.c
@@ -6,17 +6,28 @@
 void mutation(char *inputdna,char *outputdna,const int level) {
     int i;
     for(i=0; inputdna[i]  ; i++) {
-        if((rand() % 20) < level) {
-            outputdna[i] = 'X';
-        } else {
-            outputdna[i] = inputdna[i];
+        if(inputdna[i]=='A' || inputdna[i]=='G'){
+            if((rand() % 15) < level) {
+                outputdna[i] = 'X';
+            }
+            else {
+                outputdna[i] = inputdna[i];
+            }
+        }
+        else{
+            if((rand() % 20) < level) {
+                outputdna[i] = 'X';
+            }
+            else {
+                outputdna[i] = inputdna[i];
+            }
         }
     }
     
     outputdna[i]= '\0'; 
 }
 
-int mutationcounter(char *dna,const int length) {
+int mutationcounter(char *dna) {
     int count=0;
     for(; *dna; dna++) {
         if(*dna == 'X') {
@@ -27,7 +38,7 @@ int mutationcounter(char *dna,const int length) {
 }
 
 int is_survived(char *dna,const int length) {
-    int count = mutationcounter(dna, length);
+    int count = mutationcounter(dna);
     float percentage = ((float)count / length) * 100;
     
     if(percentage >= 30.0) {
@@ -37,31 +48,67 @@ int is_survived(char *dna,const int length) {
     }
 }
 
+int purines_counter (char *dna){ //purines are more exposed to damages by radiations
+    int c=0;
+    while(*dna){
+        if(*dna=='A' || *dna=='G')
+            c++;
+        dna++;
+    }
+    return c;
+}
+
+int comparator (char *dna_1,char *dna_2){
+    if(purines_counter(dna_1)>purines_counter(dna_2))
+        return 1;
+    else
+        return 0;
+}
+
 int main() {
     srand(time(NULL)); 
     
     int radiationlevel = 0;
     char dna[] = "ATGCGATACGTTGCAATCGACTGACTAGCTAGCTAGCT";
+    char dna_2[]="ATGGGCCTATTAGGCTATACGGGCTAGTAAGCCATATC";
     int len = strlen(dna);
-    char modified_dna[len + 1]; 
+    int len_2=strlen(dna_2);
+    char modified_dna[len + 1];
+    char modified_dna_2[len_2 + 1];
 
-    printf("SEQUENZA DNA ORIGINALE: %s\n", dna);
+
+    printf("FIRST ORIGINAL DNA SEQUENCE: %s\n", dna);
+    printf("SECOND ORIGINAL DNA SEQUENCE: %s\n", dna_2);
     
-    printf("Inserisci il livello di radiazioni (1-10): ");
+    printf("Insert the radiation level (1-10): ");
     do {
         scanf("%d", &radiationlevel);
     } while(radiationlevel < 1 || radiationlevel > 10);
 
+    printf("Based on the number of purines the %s dna sequence is the most susceptible to damage",comparator(dna,dna_2) ? "FIRST" : "SECOND");
+
     mutation(dna, modified_dna, radiationlevel);
+    mutation(dna_2,modified_dna_2,radiationlevel);
 
-    printf("\nSEQUENZA DNA MODIFICATA: %s", modified_dna);
-    printf("\nIn totale sono avvenute: %d MUTAZIONI", mutationcounter(modified_dna, len));
+    //output 
 
-    // Controllo sopravvivenza
+    printf("\nFIRST MODIFIED DNA SEQUENCE: %s", modified_dna);
+    printf("\nIn total there are: %d MUTATIONS", mutationcounter(modified_dna, len));
+
+    printf("\nSECOND MODIFIED DNA SEQUENCE: %s", modified_dna_2);
+    printf("\nIn total there are: %d MUTATIONS", mutationcounter(modified_dna_2, len_2));
+
+    // Check survival rate
     if(is_survived(modified_dna, len)) {
-        printf("\nESITO: La cellula e' SOPRAVVISSUTA.\n");
+        printf("\nCONCLUSION: The cell n°1 survived.\n");
     } else {
-        printf("\nESITO: La cellula e' NON SOPRAVVISSUTA (Danno critico).\n");
+        printf("\nCONCLUSION: The cell n°1 died (critical damage).\n");
+    }
+
+    if(is_survived(modified_dna_2, len_2)) {
+        printf("\nCONCLUSION: The cell n°2 survived.\n");
+    } else {
+        printf("\nCONCLUSION: The cell n°2 died (critical damage).\n");
     }
 
     return 0;
